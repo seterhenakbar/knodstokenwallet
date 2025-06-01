321b6470fdbd6766b84c1a5891dafe18b9dcc15b
Rizky Tahir <rizkytahir96@gmail.com>
2025-06-02 04:44:22 +0700

    fix: update tailwind

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">
+      <main className="grow">
         {children}
       </main>
       <Footer />
